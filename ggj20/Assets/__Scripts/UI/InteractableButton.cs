a7c38d29718b3e965d82c42a8426935cf5f6ba23
Sandeep Nambiar <sandeep@livelikevr.com>
2020-02-01 21:37:35 +0530

    Add callback to button

--- ggj20/Assets/__Scripts/UI/InteractableButton.cs
+++ ggj20/Assets/__Scripts/UI/InteractableButton.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Zenject;
+using Newtonsoft.Json;
 
 public class InteractableButton : MonoBehaviour
 {
     [SerializeField] private TMP_Text buttonText;
 
+    [Inject] private ClientListener client;
+
+    private ButtonInfo buttonInfo;
+
     public void SetButtonText(string text)
     {
         buttonText.text = text;
+    }
+
+    public void OnClick() {
+        client.SendMessage(new NetworkData {
+                Type = MessageType.RESPONSE,
+                Data = JsonConvert.SerializeObject(buttonInfo)
+            });
     }
 }
