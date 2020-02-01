734281828116bd8cb84ddb2fc337b07e96e3c092
MSI\Mohit Mittal <mittalmohitmittal93@gmail.com>
2020-02-02 01:05:18 +0530

    update progress bar with new time out as instruction changes

--- ggj20/Assets/__Scripts/StatesAndManagers/PlayArea.cs
+++ ggj20/Assets/__Scripts/StatesAndManagers/PlayArea.cs
@@ -2,16 +2,23 @@
 using Zenject;
 using UniRx;
 using TMPro;
+using System.Linq;
+using UnityEngine.UI;
 
 public class PlayArea : MonoBehaviour
 {
     public Transform buttonsPanel;
     public TMP_Text TextAreaText;
+    public Image progressBar;
 
     // [Inject] private ApplicationState applicationDataState;
     [Inject] private ClientState clientState;
     [Inject] private InteractableButton.ClickButtonFactory clickButtonFactory;
     [Inject] private InteractableButton.ToggleButtonFactory toggleButtonFactory;
+    [Inject] private LevelConfig levelConfig;
+
+    private float currentProgress;
+    private float currentMaxProgress;
 
     private void OnEnable()
     {
@@ -20,7 +27,6 @@ public class PlayArea : MonoBehaviour
 
     public void SetupPlayArea()
     {
-
         clientState.CurrentLevel
             .Where(level => level != null)
             .DistinctUntilChanged()
@@ -59,5 +65,30 @@ public class PlayArea : MonoBehaviour
             .DistinctUntilChanged()
             .TakeUntilDisable(this)
             .Subscribe(buttonInfo => TextAreaText.text = buttonInfo.ButtonTextOptions.Count > 0 ? buttonInfo.ButtonTextOptions[0] : "Not Found");//TODO:randomise
+
+        clientState.CurrentInstruction
+            .Where(buttonInfo => buttonInfo != null)
+            .DistinctUntilChanged()
+            .TakeUntilDisable(this)
+            .Subscribe(ResetProgress);
+    }
+
+    private void ResetProgress(ButtonInfo buttonInfo)
+    {
+        currentMaxProgress = levelConfig.LevelInfo[clientState.CurrentLevel.Value.Index].Timeout;
+        currentProgress = currentMaxProgress;
+    }
+
+    private void Update()
+    {
+        if(currentProgress > 0.0f)
+        {
+            currentProgress -= Time.unscaledDeltaTime;
+            progressBar.fillAmount = currentProgress / currentMaxProgress;
+        }
+        else
+        {
+            //TODO:Callback to server that instruction timed out.
+        }
     }
 }
